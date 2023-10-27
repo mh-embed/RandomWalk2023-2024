6e3569304268308b2f60a6c38556e9d391dca599
joshbrod <joshbrod@umich.edu>
2023-10-26 20:31:26 -0400

    syntax fixes

--- RandomWalk.ino
+++ RandomWalk.ino
@@ -7,27 +7,38 @@
 
 const int _rowPins[] = {CONST_ROW_0, CONST_ROW_1};
 const int _colPins[] = {CONST_COL_0, CONST_COL_1};
-LedMatrix matrix;
+
+int* arrayDeepCopy(const int arr1[]){
+  int size = sizeof(arr1)/sizeof(int);
+  int* arr2 = new int[size];
+  for(int i = 0; i < size; i++){
+    arr2[i] = arr1[i];
+  }
+  return arr2;
+}
 
 class LedMatrix
 {
   static const int NUM_ROWS = 2;
-  static const int NUM_COLS = 2;
-
+  static const int NUM_COLS = 2; 
+  //int rowPins[NUM_ROWS];
 public:
-  LedMatrix(const int rowPins[], const int colPins[]) : rowOn(-1), colOn(-1)
+  LedMatrix(const int input_row[],const int input_col[]) : rowOn(-1), colOn(-1)
   {
-    for (int row : rowPins) pinMode(row, OUTPUT);
-    for (int col : colPins) pinMode(col, OUTPUT);
-
-    this->rowPins = rowPins;
-    this->colPins = colPins;
+    for (int i = 0; i < NUM_ROWS; i++){
+      pinMode(input_row[i], OUTPUT);
+    } 
+    for (int j = 0; j < NUM_COLS; j++) {
+      pinMode(input_col[j], OUTPUT);
+    }
+    rowPins = arrayDeepCopy(input_row);
+    colPins = arrayDeepCopy(input_col);
   }
   
     void turnOn(int row, int col)
     {
       this->clear();
-      digitalWrite(this->rowPins[row], HIGH);
+      digitalWrite(this->rowPins[row], HIGH); // We can rewrite this as a Setter function for setting individual rowPins
       digitalWrite(this->colPins[col], HIGH);
       this->rowOn = row;
       this->colOn = col;
@@ -52,17 +63,19 @@ public:
     }
 
   private:
-    const int rowPins[NUM_ROWS];
-    const int colPins[NUM_COLS];
+    int* rowPins;
+    int* colPins;
     // These are -1 when no LED is active
     int rowOn;
     int colOn;
   };
 
+  LedMatrix matrix = LedMatrix(_rowPins, _colPins);
+
   // Set up for eight Digital Pins
   void setup()
   {
-    matrix = LedMatrix(_rowPins, _colPins);
+    Serial.begin(9600);
   }
 
   void loop()
@@ -74,3 +87,4 @@ public:
       }
     }
   }
+
